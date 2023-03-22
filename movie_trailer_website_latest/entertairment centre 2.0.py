e9c3247f14c7e387b7e85e068d4da25f9abd6138
tanishq <tanishq896.be22@chitkara,edu.in>
2023-03-22 14:41:28 +0530

    first commit

---
+++ movie_trailer_website_latest/entertairment centre 2.0.py
@@ -0,0 +1,112 @@
+import movie_trailer
+import media
+""" import function is an inbuilt function which imports code from outside
+this file (that file can be any other python file or web browser or
+information from your computer like date and time etc. ).
+for e.g. in tis file, we are importing our code fom
+movie_trailer.py and media.py.
+"""
+
+Brahmastra = media.Movie("Brahmastra"," Adventure, Action","2022, IMDb Rating 5.6*",
+"https://upload.wikimedia.org/wikipedia/en/4/40/Brahmastra_Teaser.jpeg",
+"https://www.youtube.com/watch?v=BUjXzrgntcY")
+# self is always skipped
+Yeh_Jawaani_Hai_Deewani = media.Movie("Yeh Jaawani Hai Deewani",
+                                      "Romance, Drama, Family, Hindi","2013, IMDb Rating 7.2*",
+                                      "https://upload.wikimedia.org/wikipedia/en/1/15/Yeh_jawani_hai_deewani.jpg",
+                                       "https://www.youtube.com/watch?v=Rbp2XUSeUNE")
+
+FaultInStars = media.Movie("The Fault In Our Stars", "Youth, Adult, Fiction",
+"2014, IMDb Rating 7.7*", "https://upload.wikimedia.org/wikipedia/en/thumb/4/41/The_Fault_in_Our_Stars_%28Official_Film_Poster%29.png/220px-The_Fault_in_Our_Stars_%28Official_Film_Poster%29.png",
+"https://www.youtube.com/watch?v=9ItBvH5J6ss")
+
+Thor = media.Movie("Thor", "Action, Adventure, Fantacy", "2011, IMDb Rating 7.0*",
+"https://upload.wikimedia.org/wikipedia/en/9/95/Thor_%28film%29_poster.jpg",
+"https://www.youtube.com/watch?v=JOddp-nlNvQ")
+
+Titanic = media.Movie("Titanic", "Romance, Disaster, Historical, Epic, Drama",
+"1997, IMDb Rating 7.9*", "https://upload.wikimedia.org/wikipedia/en/1/18/Titanic_%281997_film%29_poster.png",
+"https://www.youtube.com/watch?v=2e-eXJ6HgkQ")
+
+Frozen = media.Movie("Frozen", "Animation, Comedy, Adventure, Fantacy", "2013, IMDb Rating 7.4*",
+"https://upload.wikimedia.org/wikipedia/en/thumb/0/05/Frozen_%282013_film%29_poster.jpg/220px-Frozen_%282013_film%29_poster.jpg",
+"https://www.youtube.com/watch?v=Adpi_tMDQvA")
+
+JungleBook = media.Movie("The Jungle Book",
+"Animation, Comedy, Adventure, Drama", "1967, IMDb Rating 7.6*",
+"https://upload.wikimedia.org/wikipedia/en/thumb/1/1d/Thejunglebook_movieposter.jpg/220px-Thejunglebook_movieposter.jpg",
+"https://www.youtube.com/watch?v=LNVTKXIK7q8")
+
+
+IronMan3 = media.Movie("Iron Man 3",
+"Action, Superhero, Adventure, Science", "2013, IMDb Rating 7.1*",
+"https://upload.wikimedia.org/wikipedia/en/1/19/Iron_Man_3_poster.jpg",
+"https://www.youtube.com/watch?v=muIsc5lIEyQ")
+
+bhediya = media.Movie("Bhediya",
+                      "Action, Horror","2022, IMDb Rating 7.9*",
+                      "https://upload.wikimedia.org/wikipedia/en/2/29/Bhediya_film_poster.jpg",
+                      "https://www.youtube.com/watch?v=d_BPlvbw_ok")
+Angry_Bird = media.Movie("The Angry Bird",
+"Animation, Comedy, Action, Family", "2016, IMDb Rating 6.3*",
+"https://upload.wikimedia.org/wikipedia/en/thumb/f/f9/The_Angry_Birds_Movie_poster.png/220px-The_Angry_Birds_Movie_poster.png",
+"https://www.youtube.com/watch?v=e4sdQBmqnuA")
+Avengers = media.Movie("The Avengers",
+"Action, Adventure, Science, Superhero", "2015, IMDb Rating 7.3*",
+"https://upload.wikimedia.org/wikipedia/en/8/8a/The_Avengers_%282012_film%29_poster.jpg",
+"https://www.youtube.com/watch?v=eOrNdBpGMv8")
+idiots = media.Movie("3 Idiots",
+                       "Comedy, Romance, Family","2009, IMDb Rating 8.4*",
+                       "https://upload.wikimedia.org/wikipedia/en/d/df/3_idiots_poster.jpg",
+                       "https://www.youtube.com/watch?v=K0eDlFX9GMc")
+Jab_Tak_Hai_Jaan = media.Movie("Jab Tak Hai Jaan",
+                               "Romance, Drama","2012, IMDb Rating 6.7*",
+                               "https://upload.wikimedia.org/wikipedia/en/f/f9/Jab_Tak_Hai_Jaan_Poster.jpg",
+                               "https://www.youtube.com/watch?v=v0UXgoJ9Shg")
+Student_of_The_Year = media.Movie("Student of The Year",
+                                  "Romance, Comedy, Drama","2012, IMDb Rating 5.2*",
+                                  "https://upload.wikimedia.org/wikipedia/en/0/00/Student_of_the_Year_Poster.jpg",
+                                  "https://www.youtube.com/watch?v=fivOhPjX9YM")
+Sanam_Teri_Kasam = media.Movie("Sanam Teri Kasam",
+                               "Romance, Drama","2016, IMDb Rating 7.5*",
+                               "https://upload.wikimedia.org/wikipedia/en/7/72/Sanam_Teri_Kasam_2016.jpeg",
+                               "https://www.youtube.com/watch?v=1IpBoMWRjm8")
+Jaat_Brothers = media.Movie("Jaat Brothers",
+                            "Comedy, Punjabi, Family", "2022, IMDb Rating 7.4*",
+                            "https://upload.wikimedia.org/wikipedia/en/2/2e/Jatt_Brothers_Poster.jpeg",
+                            "https://www.youtube.com/watch?v=A9DoATjDQMc")
+Shiddat = media.Movie("Shiddat",
+                      "Romance, Drama","2021, IMDb Rating 7.6*",
+                      "https://upload.wikimedia.org/wikipedia/en/5/5e/Shiddat_poster.jpg",
+                      "https://www.youtube.com/watch?v=x60tMH7Pz9s")
+Five_seat_Apart = media.Movie("Five Seat Apart","Drama, Romance, Inspirational", "2019, IMDb Rating 5.2*",
+                              "https://upload.wikimedia.org/wikipedia/en/a/a7/Five_Feet_Apart_%282019_poster%29.png",
+                              "https://www.youtube.com/watch?v=XtgCqMZofqM")
+jannat = media.Movie("Jannat", "Romance, Drama", "2008, IMDb Rating 6.3*",
+                     "https://upload.wikimedia.org/wikipedia/en/6/61/Jannat_poster.jpg",
+                     "https://www.youtube.com/watch?v=K9n-aMlLJlo&t=1s")
+drishyam_2 = media.Movie("Drishyam 2","Mystery", "2022, IMDb Rating 8.7*",
+                         "https://upload.wikimedia.org/wikipedia/en/9/9e/Drishyam_2_2022_film_poster.jpg",
+                         "https://www.youtube.com/watch?v=cxA2y9Tgl7o")
+hosla_rakh = media.Movie("Honsla Rakh", "Romance, Comedy, Punjabi",
+                         "2021, IMDb Rating 7.3*", "https://upload.wikimedia.org/wikipedia/en/7/70/Honsla_Rakh_film.jpg",
+                                 "https://www.youtube.com/watch?v=")
+bahubali = media.Movie("Baahubali", " Action, War", "2015, IMDb Rating 8.0*",
+                       "https://upload.wikimedia.org/wikipedia/en/5/5f/Baahubali_The_Beginning_poster.jpg",
+                       "https://www.youtube.com/watch?v=sOEg_YZQsTI")
+ajab_prem_ki_gajab_kahani = media.Movie("Ajab Prem Ki Ghazab Kahani","Romance, Comedy, Drama", "2009, IMDb Rating 6.4*",
+                                        "https://upload.wikimedia.org/wikipedia/en/6/67/Ajab_Prem_Ki_Ghazab_Kahani.jpg",
+                                        "https://www.youtube.com/watch?v=Ag4N6rCj0VA")
+Actor = media.Movie("An Actor Hero","Action, Comedy, Thriller","2022, IMDb Rating 8.2*",
+                    "https://upload.wikimedia.org/wikipedia/en/4/47/An_Action_Hero_film_poster.jpg",
+                    "https://www.youtube.com/watch?v=vCTt_PnWHPg")
+Honeymoon = media.Movie("Honeymoon", "Comedy, Romance, Punjabi","2022, IMDb Rating 6.2*",
+                        "https://upload.wikimedia.org/wikipedia/en/3/39/Honeymoon_%282022%29_film_poster.jpg",
+                        "https://www.youtube.com/watch?v=Z0uAhXV9Wpc")
+
+
+movies = [drishyam_2, Brahmastra, bhediya, Jaat_Brothers, Shiddat, hosla_rakh, Five_seat_Apart,
+          Sanam_Teri_Kasam, Angry_Bird, Avengers, bahubali, FaultInStars, IronMan3, Yeh_Jawaani_Hai_Deewani, Frozen,
+          Student_of_The_Year, Jab_Tak_Hai_Jaan, Thor, ajab_prem_ki_gajab_kahani, idiots, jannat, Titanic, JungleBook]
+
+movie_trailer.open_movies_page(movies)
